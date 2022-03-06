3c7d0e7d33a09e281b4daff5d346936c45116c75
Zaidkhan7 <67749877+Zaidkhan7@users.noreply.github.com>
2022-03-06 21:39:55 +0530

    Add files via upload

---
+++ flutter projects/bookmyshowclone/main.dart
@@ -0,0 +1,11 @@
+import 'package:bookmyshow/Screens/bookmyshow.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:Bookmyshow() ,
+    
+  ));
+}
+
